a866857304930e5db1c6be93f1b77734280f2eae
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2026-01-03 16:30:25 +0100

    feat: add test for LeafNode methods

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -23,3 +23,22 @@ class TestHTMLNode(unittest.TestCase):
         output = self.head_node.children_to_string()
         expected = 'HTMLNode(tag: title, value: Document, children: None, attributes:)'
         self.assertEqual(output, expected)
+
+class TestLeafNode(unittest.TestCase):
+    def setUp(self):
+        self.paragraph_node = LeafNode(tag="p", value="Hello, world!")
+        self.anchor_node = LeafNode(tag="a", value="Click me!", props={"href":"https://www.google.com"})
+        self.text_node = LeafNode(tag=None, value="Raw text")
+        self.wrong_leafnode = LeafNode(tag=None, value=None)
+
+    def test_leaf_to_html_p(self):
+        self.assertEqual(self.paragraph_node.to_html(), "<p>Hello, world!</p>")
+    
+    def test_leaf_with_attr_to_html(self):
+        self.assertEqual(self.anchor_node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+    
+    def test_leaf_with_text_only(self):
+        self.assertEqual(self.text_node.to_html(), "Raw text")
+    
+    def test_wrong_leaf(self):
+        self.assertRaises(ValueError, self.wrong_leafnode.to_html)
