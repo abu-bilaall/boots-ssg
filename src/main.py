e5fce4d1b26b39ac752b8ae3cb16825edec4ff76
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2026-02-06 12:31:23 +0100

    feat: change dest dir to docs

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
+import sys
 from src_to_dest import src_to_dest
+from generate_page import generate_pages_recursive
 
 def main():
-    src_to_dest("./static", "./public")
+    src_to_dest("./static", "./docs")
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+    generate_pages_recursive("./content", "./template.html", "./docs", basepath)
 
-main()
+if __name__ == "__main__":
+    main()
